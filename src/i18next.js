7daab53dd6ef1c2641dac8940c0829ae77ce274c
IrinaChoulgan <irina_choulgan@icloud.com>
2023-02-24 23:45:47 +0100

    fix translation settings

--- src/i18next.js
+++ src/i18next.js
@@ -2,17 +2,6 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import translationEN from './locales/en/translation.json';
-import translationNL from './locales/nl/translation.json';
-
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  nl: {
-    translation: translationNL,
-  },
-};
 
 i18n
   .use(Backend)
@@ -21,7 +10,6 @@ i18n
   .init({
     fallbackLng: 'uk',
     debug: true,
-    resources,
     detection: {
       order: ['localStorage', 'cookie'],
       caches: ['localStorage', 'cookie'],
