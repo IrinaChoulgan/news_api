f272c05d2f77aec3572d36a6d87292047540edfa
IrinaChoulgan <irina_choulgan@icloud.com>
2023-02-24 23:35:53 +0100

    fix translation for gh-pages deploy

--- src/i18next.js
+++ src/i18next.js
@@ -2,6 +2,17 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import translationEN from './locales/en/translation.json';
+import translationNL from './locales/nl/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  nl: {
+    translation: translationNL,
+  },
+};
 
 i18n
   .use(Backend)
@@ -10,13 +21,14 @@ i18n
   .init({
     fallbackLng: 'uk',
     debug: true,
+    resources,
     detection: {
       order: ['localStorage', 'cookie'],
-      caches: ['localStorage', 'cookie']
+      caches: ['localStorage', 'cookie'],
     },
     interpolation: {
-      escapeValue: false
-    }
+      escapeValue: false,
+    },
   });
 
 export default i18n;
