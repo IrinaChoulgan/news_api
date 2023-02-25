cecdf37a39da296f3af82ce9fc5b1dbea4794655
IrinaChoulgan <irina_choulgan@icloud.com>
2023-02-25 11:35:27 +0100

    add settings i18next

--- src/i18next.js
+++ src/i18next.js
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import XHR from 'i18next-xhr-backend';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
@@ -8,6 +9,23 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    loadPath: '/locales/{{lng}}/{{ns}}.json',
+    addPath: 'locales/add/{{lng}}/{{ns}}',
+    allowMultiLoading: false,
+    parse: function (data) {
+      return data.replace(/a/g, '');
+    },
+    parsePayload: function (namespace, key, fallbackValue) {
+      return { key };
+    },
+    crossDomain: false,
+    withCredentials: false,
+    overrideMimeType: false,
+    customHeaders: {
+      authorization: 'foo',
+    },
+    queryStringParams: { v: '1.3.5' },
+
     fallbackLng: 'uk',
     debug: true,
     detection: {
